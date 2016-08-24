3db295e37dac0de074fe36a35e0c24bc37814a87
Tanmay <me@localhost.com>
2016-08-24 20:25:45 +0530

    Basic Files

---
+++ sarkar/src/main/java/tanmay/sarkar/App.java
@@ -0,0 +1,13 @@
+package tanmay.sarkar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
